c901f10c10329f34e6069038c7f12e0c5f56a77f
Mathieu <ribout.mathieu@gmail.com>
2024-09-18 16:51:39 +0200

    push

---
+++ test_sample.py
@@ -0,0 +1,5 @@
+def lucas(x:int,y:int) -> int:
+    return x+y+1
+
+def test_lucas():
+    assert lucas(1,2) == 34575
---
+++ test_toto.py
@@ -0,0 +1,5 @@
+def toto(x,y,z):
+    return (x*y)**z
+
+def test_toto():
+    assert toto(1,1,0) == 1
